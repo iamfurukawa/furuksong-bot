0125eee4974c92229a64834552b423b3c2785b9d
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2021-07-14 12:17:20 -0300

    Bugfix:  - Tratamento pra pause e melhoria de log.  - Agora aceita nomes com espaço. New features:  - /desculpinha retorna uma desculpinha aleatoria

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'
 import Discord from 'discord.js'
 import Sounds from './sounds'
 import FirebaseRealtimeService from './firebase/firebase-realtime-service'
@@ -11,31 +12,81 @@ import DiscordUtils from './discord/utils'
     const botId = (await FirebaseFirestoreService.configurations()).data().bot_id
     const client = new Discord.Client()
     client.login(botId)
+
     let conn = null
     let lastSoundPlayed = null
+    let actualSound = null
+    let isSpeaking = false
+
+    const problemsMessages = [
+      "É coisa da cdt!",
+      "É coisa de infra",
+      "Tava arrumando o fone",
+      "Tava arrumando o monitor",
+      "Hoje foi enrolado, passei o dia inteiro em call",
+      "Qual é o numero da gmud? Eu não to localizando",
+      "Aff ta em ingles",
+      "Meu teclado ta ruim",
+      "Opa gente desculpa ai, tocou o telefone enquanto eu falava com vocês",
+      "Hoje minha VPN ta mto ruim",
+      "To com prolema no meu AWS aqui, já vou dar um jeito",
+    ]
+
+    const getRandomInt = (min = 0, max = (problemsMessages.length - 1)) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    client.on('message', message => {
+      if (message.content === '/desculpinha') {
+        message.channel.send(`${problemsMessages[getRandomInt()]}`)
+      }
+    })
 
     client.on("ready", async () => {
       monitoringRef.on('value', async (snap) => {
-        const [uuid, userId] = (snap.val().RequestSoundMonitoring).split(' ')
+        const data = snap.val().RequestSoundMonitoring
+        let [uuid, userId, ip] = data.split(' ')
+
         if (uuid === 'awaiting-request-song') return
 
-        const location = DiscordUtils.findChannelByDiscordUser(client.guilds.cache, 'iamFurukawa#1968'/*userId*/)
-        if (!location) {
-          console.log(`User ${userId} not found.`)
+        if (lastSoundPlayed && lastSoundPlayed.uuid == uuid && isSpeaking) {
+          actualSound.pause()
+          lastSoundPlayed = null
+          actualSound = null
+          isSpeaking = false
           await FirebaseRealtimeService.resetRequestSoundMonitoring()
+          console.log(`[${new Date().toLocaleString()}] [${ip}] [${userId}] Pausing sound.`)
           return
         }
+        userId = userId.split('¬').join(' ')
+        const location = DiscordUtils.findChannelByDiscordUser(client.guilds.cache, userId)
+        if (!location) {
+          await FirebaseRealtimeService.resetRequestSoundMonitoring()
+          console.log(`User ${userId} not found.`)
+          console.log(`DEBUG: ${data}`)
+          return
+        }
+
+        actualSound ? actualSound.pause() : () => { }
+        lastSoundPlayed = null
+        actualSound = null
+        isSpeaking = false
 
         conn = await location.channel.join()
         conn.voice.setSelfDeaf(true)
 
         lastSoundPlayed = sounds.soundsFirebase.find(sound => sound.uuid === uuid)
-        console.log(`User ${userId} request sound: ${lastSoundPlayed.displayName}`)
-        conn.play(sounds.path + lastSoundPlayed.originalName)
+        console.log(`[${new Date().toLocaleString()}] [${ip}] [${userId}] Request: ${lastSoundPlayed.displayName}`)
+        actualSound = conn.play(sounds.path + lastSoundPlayed.originalName)
 
         await FirebaseFirestoreService.incrementPlayedTimes(lastSoundPlayed.uuid)
         await FirebaseRealtimeService.resetRequestSoundMonitoring()
-        //await guild.channels.cache.filter(channel => channel.type === 'voice').get('759035649998454794').leave()//sair
+
+        actualSound.on("speaking", (speaking) => {
+          isSpeaking = speaking
+        });
       })
     })
 
--- src/sounds/index.js
+++ src/sounds/index.js
@@ -25,34 +25,39 @@ const Sounds = {
     }
 
     const _create = (sounds = []) => {
-      if (sounds.length === 0) return
+      if (sounds.length === 0) return false
       console.log(`To add: ${sounds}`)
 
       sounds.forEach(sound => {
         FirebaseFirestoreService.save('sounds', uuidv4(), {
           playedTimes: 0,
           originalName: sound,
-          displayName: sound.replace('/.mp3/g', '').replace(/'-'/g, ' ').replace(/'_'/g, ' ').trim()
+          displayName: sound.replace(/(\.[^/.]+)+$/, '').replace(/-/g, ' ').replace(/_/g, ' ').trim()
         })
       })
+
+      return true
     }
 
     const _remove = (sounds = []) => {
-      if (sounds.length === 0) return
+      if (sounds.length === 0) return false
       console.log(`To remove: ${sounds}`)
 
       sounds.forEach(async sound => {
         const soundObj = this.soundsFirebase.find(s => s.originalName === sound)
         await FirebaseFirestoreService.remove('sounds', soundObj.uuid)
       })
+
+      return true
     }
 
     await _init()
 
-    _create(this.soundsLocal.filter(soundLocal => !this.soundsFirebase.map(soundsFirebaseObj => soundsFirebaseObj.originalName).includes(soundLocal)))
-    _remove(this.soundsFirebase.map(soundsFirebaseObj => soundsFirebaseObj.originalName).filter(soundFirebase => !this.soundsLocal.includes(soundFirebase)))
+    const createStatus = _create(this.soundsLocal.filter(soundLocal => !this.soundsFirebase.map(soundsFirebaseObj => soundsFirebaseObj.originalName).includes(soundLocal)))
+    const removeStatus = _remove(this.soundsFirebase.map(soundsFirebaseObj => soundsFirebaseObj.originalName).filter(soundFirebase => !this.soundsLocal.includes(soundFirebase)))
 
-    await _init()
+    if (createStatus || removeStatus)
+      await _init()
 
     return this
   }
